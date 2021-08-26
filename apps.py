b2d56b22d9edc359343d51b855a111d2e4def28a
abhinav0606 <abhinavgangrade0606@gmail.com>
2021-08-26 12:03:17 +0530

    Added Data Generation to apps.py

--- apps.py
+++ apps.py
@@ -1,9 +1,14 @@
-from flask import Flask,render_template,Response
+from flask import Flask
+from flask import render_template
+from flask import Response
+from flask import request
 import cv2
+import os
 app=Flask(__name__)
 faces=cv2.CascadeClassifier("frontal_face.xml")
-def gen_dataset():
-    capture = cv2.VideoCapture(0)
+def gen_dataset(folder):
+    capture = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    count=1
     while True:
         success, frame = capture.read()
         frame=cv2.flip(frame,1)
@@ -14,14 +19,36 @@ def gen_dataset():
         else:
             for (x,y,w,h) in face:
                 frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+                cv2.imwrite(r"C:\Users\\abhin\Desktop\Projects\Attendance-Taking-System-with-the-help-of-python-Opencv-\\" + folder + "\\" + str(count) + ".JPG", cv2.cvtColor(frame[y:y + h, x:x + w], cv2.COLOR_BGR2GRAY))
+                count = count + 1
                 ret, buffer = cv2.imencode('.jpg', frame)
                 frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                if count==400:
+                    break
+                else:
+                    yield (b'--frame\r\n'
+                           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        if count==400:
+            break
+    capture.release()
+    cv2.destroyAllWindows()
+    return "Done!"
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route("/",methods=["POST"])
+def index_post():
+    global folder
+    college_id=request.form.get("ids")
+    folder=college_id.lower()
+    try:
+        os.mkdir(r"C:\Users\\abhin\Desktop\Projects\Attendance-Taking-System-with-the-help-of-python-Opencv-\\" + folder)
+    except:
+        return "Folder Already Exist!"
+    return render_template("login_view.html",folder=folder)
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_dataset(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # return name
+    return Response(gen_dataset(folder), mimetype='multipart/x-mixed-replace; boundary=frame')
 app.run(debug=True)
