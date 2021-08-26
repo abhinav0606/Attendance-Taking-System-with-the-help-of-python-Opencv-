884257dc6672f241bfd62f690736f91372ff2ea6
abhinav0606 <abhinavgangrade0606@gmail.com>
2021-08-26 13:54:34 +0530

    Added Register Interface

--- apps.py
+++ apps.py
@@ -10,13 +10,10 @@ def gen_dataset(folder):
     capture = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     count=1
     while True:
-        success, frame = capture.read()
-        frame=cv2.flip(frame,1)
-        grey_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        face=faces.detectMultiScale(grey_frame,1.3,5)
-        if not success:
-            break
-        else:
+            success, frame = capture.read()
+            frame=cv2.flip(frame,1)
+            grey_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            face=faces.detectMultiScale(grey_frame,1.3,5)
             for (x,y,w,h) in face:
                 frame = cv2.putText(frame, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), thickness=2)
                 frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
@@ -29,8 +26,8 @@ def gen_dataset(folder):
                 else:
                     yield (b'--frame\r\n'
                            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        if count==400:
-            break
+            if count==400:
+                break
     capture.release()
     cv2.destroyAllWindows()
     return "Done!"
@@ -39,6 +36,18 @@ def index():
     return render_template('index.html')
 @app.route("/",methods=["POST"])
 def index_post():
+    return "Under Construction"
+
+@app.route('/video_feed')
+def video_feed():
+    # return name
+    return Response(gen_dataset(folder), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+@app.route("/register",methods=["POST"])
+def register_post():
     global folder
     college_id=request.form.get("ids")
     folder=college_id.lower()
@@ -46,10 +55,5 @@ def index_post():
         os.mkdir(r"C:\Users\\abhin\Desktop\Projects\Attendance-Taking-System-with-the-help-of-python-Opencv-\\" + folder)
     except:
         return "Folder Already Exist!"
-    return render_template("login_view.html",folder=folder)
-
-@app.route('/video_feed')
-def video_feed():
-    # return name
-    return Response(gen_dataset(folder), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return render_template("register_view.html",folder=folder)
 app.run(debug=True)
