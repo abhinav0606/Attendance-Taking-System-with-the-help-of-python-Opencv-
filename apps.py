4bc5f40296feb33c8eae7221a8433faa06824796
abhinav0606 <abhinavgangrade0606@gmail.com>
2021-08-26 12:27:24 +0530

    Added Put Text in frame

--- apps.py
+++ apps.py
@@ -18,6 +18,7 @@ def gen_dataset(folder):
             break
         else:
             for (x,y,w,h) in face:
+                frame = cv2.putText(frame, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), thickness=2)
                 frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
                 cv2.imwrite(r"C:\Users\\abhin\Desktop\Projects\Attendance-Taking-System-with-the-help-of-python-Opencv-\\" + folder + "\\" + str(count) + ".JPG", cv2.cvtColor(frame[y:y + h, x:x + w], cv2.COLOR_BGR2GRAY))
                 count = count + 1
